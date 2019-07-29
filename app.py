b7c2da88dea0587bcaa817a030332ac562ec70b9
ismailKtami <ktamiismail@gmail.com>
2019-07-29 03:21:04 +0100

    commit last

--- app.py
+++ app.py
@@ -20,9 +20,5 @@ api.add_resource(ItemListOfStore,'/<string:name>/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserReister,'/user/register')
 
-
-
 if __name__=='__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000,debug=True)
